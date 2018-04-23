4d22a774975300e142d4fb10bf1aa21881c8f7e0
Gershon Burov <304315146@gov.il>
2018-04-23 17:25:45 +0300

    23-04-2018_1

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+
+const API_KEY = '7330ac9a49aa0efaf1e783f25f5bea70';
+export const FETCH_WEATHER = 'fetchweather';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export function fetchWeather(city)
+{
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    return{
+        type:FETCH_WEATHER,
+        payload:request
+    }
+}
---
+++ src/containers/search-bar.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import fetchWeather from '../actions/index';
+
+class SearchBar extends Component{
+
+
+    constructor(props){
+        super(props);
+        this.state = {term:''};
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event){
+        this.setState({term:event.target.value});
+    }
+
+    onformSubmit(event){
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <form className="input-group" onSubmit={this.onformSubmit}>
+                <input 
+                    placeholder="Get five day forecast"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        );
+    }
+}
+
+function mapDispachToPromise(dispatch){
+    return bindActionCreators({fetchWeather},dispatch);
+}
+
+export default connect(null,mapDispachToPromise)(SearchBar);
